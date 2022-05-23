245ea9af190e5b04a2660e600a42e38fc0837eab
paul-gansca <paul.gansca@qubiz.com>
2022-05-23 17:02:40 +0300

    Build edge function to stop requests early if missing auth token

--- pages/_middleware.ts
+++ pages/_middleware.ts
@@ -5,6 +5,7 @@ const signedInPages = ["/", "/playlist", "/library"];
 export default function middleware(req) {
   if (signedInPages.find((p) => p === req.nextUrl.pathname)) {
     const token = req.cookies.TRAX_ACCESS_TOKEN;
+
     if (!token) {
       return NextResponse.redirect("/signin");
     }
--- pages/api/signup.ts
+++ pages/api/signup.ts
@@ -35,7 +35,7 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
 
   res.setHeader(
     "Set-Cookie",
-    cookie.serialize("TRAX_ACESS_TOKEN", token, {
+    cookie.serialize("TRAX_ACCESS_TOKEN", token, {
       httpOnly: true,
       maxAge: 8 * 60 * 60,
       path: "/",
